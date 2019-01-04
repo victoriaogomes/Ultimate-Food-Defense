b7ceeb1eef43b43db0dfa78b8eabc32ef4800e13
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2019-01-04 20:04:20 -0300

    Mais uma arrumadinha no menu

---
+++ UltimateFruitDefense/bananaStaticBody2D.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+onready var body = get_node("bananaBox")
+var base_sprite = preload("res://sprites/Banana box.png")
+var hover_sprite = preload("res://sprites/Wheel.png")
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_bananaStaticBody2D_mouse_entered():
+	print("enter")
+	body.set_texture(hover_sprite)
+
+
+func _on_bananaStaticBody2D_mouse_exited():
+	print("exit")
+	body.set_texture(base_sprite)
---
+++ UltimateFruitDefense/cucumberStaticBody2D.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+onready var body = get_node("cucumberBox")
+var base_sprite = preload("res://sprites/Cucumber box.png")
+var hover_sprite = preload("res://sprites/Wheel.png")
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_cucumberStaticBody2D_mouse_entered():
+	print("enter")
+	body.set_texture(hover_sprite)
+
+
+func _on_cucumberStaticBody2D_mouse_exited():
+	print("exit")
+	body.set_texture(base_sprite)
