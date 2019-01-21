077c204f3e96d8b7f01ba30a12e0e4a345b9272e
Bruno Claudino <brunoclaudinomatias@gmail.com>
2019-01-21 16:23:08 -0300

    c

--- UltimateFruitDefense/Scenes/gameScreen/Node2D.gd
+++ UltimateFruitDefense/Scenes/gameScreen/Node2D.gd
@@ -3,8 +3,8 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-"res://Scenes/gameScreen/Mob.tscn"
-export (PackedScene) var Mob
+onready var Mob = preload("res://Scenes/gameScreen/Mob.tscn")
+#export (PackedScene) var Mob
 var score
 
 
@@ -16,9 +16,9 @@ func _ready():
 
 func on_Mob_TimeOut():
 	# choose a random location on Path2D
-    
+    $MobPath/MobSpawnLocation.set_offset(randi())
     # create a Mob instance and add it to the scene
     var mob = Mob.instance()
     add_child(mob)
     # set the mob's position to a random location
-    mob.position = $MobPath/MobSpawnLocation.set_offset(randi())
+    mob.position = $MobPath/MobSpawnLocation.position
