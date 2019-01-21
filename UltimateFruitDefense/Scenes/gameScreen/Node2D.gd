70de282594022b6446f488f43d976d2713aa4941
Bruno Claudino <brunoclaudinomatias@gmail.com>
2019-01-21 15:29:27 -0300

    Revert "apagando"
    This reverts commit 7792b7604dae556c115fe037c81feb994a577419.

--- UltimateFruitDefense/Scenes/gameScreen/Node2D.gd
+++ UltimateFruitDefense/Scenes/gameScreen/Node2D.gd
@@ -18,7 +18,7 @@ func on_Mob_TimeOut():
 	# choose a random location on Path2D
     
     # create a Mob instance and add it to the scene
-   var mob = Mob.instance()
+    var mob = Mob.instance()
     add_child(mob)
     # set the mob's position to a random location
     mob.position = $MobPath/MobSpawnLocation.set_offset(randi())
