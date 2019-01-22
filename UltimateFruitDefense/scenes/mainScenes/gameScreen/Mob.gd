eb2b83f56f7055556702323e2614ad546e8f1c9e
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2019-01-22 11:11:37 -0300

    colisão funcionando

--- UltimateFruitDefense/scenes/mainScenes/gameScreen/Mob.gd
+++ UltimateFruitDefense/scenes/mainScenes/gameScreen/Mob.gd
@@ -1,36 +1,41 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var select
+var velocity = Vector2(-150, 0)
 
 func _ready():
 	self.set_process(true)
+	self.set_physics_process(true)
+	select = randi()%5+1
+	#print(selec)
+	match(select):
+		1:
+			var pos = Vector2(600, 95)
+			self.set_position(pos)
+		2:
+			var pos = Vector2(600, 162)
+			self.set_position(pos)
+		3:
+			var pos = Vector2(600, 220)
+			self.set_position(pos)
+		4:
+			var pos = Vector2(600, 288)
+			self.set_position(pos)
+		5:
+			var pos = Vector2(600, 150)
+			self.set_position(pos)
 
-	var selec = randi()%5+1
-	print(selec)
-	if(selec==1):
-		var pos = Vector2(600, 95)
-		self.set_position(pos)
-	elif(selec==2):
-		var pos = Vector2(600, 162)
-		self.set_position(pos)
-	elif(selec==3):
-		var pos = Vector2(600, 220)
-		self.set_position(pos)
-	if(selec==4):
-		var pos = Vector2(600, 288)
-		self.set_position(pos)
-	elif(selec==5):
-		var pos = Vector2(600, 150)
-		self.set_position(pos)
-	pass
+
+func _physics_process(delta):
+	#var pos = self.get_position()
+	#pos.x -=1
+	#self.set_position(pos)
+	var collision_info = move_and_collide(velocity*delta)
+	if collision_info:
+		velocity = Vector2(0,0)
+
 
 #func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-func _process(delta):
-	var pos = self.get_position()
-	pos.x -=1
-	self.set_position(pos)
+	#var pos = self.get_position()
+	#pos.x -=1
+	#self.set_position(pos)
