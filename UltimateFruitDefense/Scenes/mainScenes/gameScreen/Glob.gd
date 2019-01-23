d6041184cfbc07f595804e536cef179234160837
Bruno Claudino <brunoclaudinomatias@gmail.com>
2019-01-23 13:54:40 -0300

    globulo na gamescene

---
+++ UltimateFruitDefense/Scenes/mainScenes/gameScreen/Glob.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization h
+	var position = Vector2(160,160)
+	self.set_position(position)
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
--- UltimateFruitDefense/scenes/mainScenes/gameScreen/Node2D.gd
+++ UltimateFruitDefense/scenes/mainScenes/gameScreen/Node2D.gd
@@ -2,14 +2,17 @@ extends Node2D
 
 #onready var Mob = preload("res://scenes/mainScenes/gameScreen/Burguer.tscn")
 onready var enemies_types = [preload("res://scenes/mainScenes/gameScreen/burguer.tscn"), preload("res://scenes/mainScenes/gameScreen/pizza.tscn"), preload("res://scenes/mainScenes/gameScreen/soda.tscn")]
+onready var glob = preload("res://Scenes/mainScenes/gameScreen/Glob.tscn")
 
 var score
 var mob
 var waitTime
 var select
-
+var soldier
 
 func _ready():
+	soldier = glob.instance()
+	add_child(soldier)
 	waitTime = 1
 	randomize()
 	$MobTimer.start()
