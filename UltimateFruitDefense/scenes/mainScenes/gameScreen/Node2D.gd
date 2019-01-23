491837add20a32d4591b0a9cf6cedd769f7a2cbc
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2019-01-23 10:05:29 -0300

    collisionPolygon2D do refri

--- UltimateFruitDefense/scenes/mainScenes/gameScreen/Node2D.gd
+++ UltimateFruitDefense/scenes/mainScenes/gameScreen/Node2D.gd
@@ -1,28 +1,22 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 onready var Mob = preload("res://scenes/mainScenes/gameScreen/Burguer.tscn")
-#export (PackedScene) var Mob
+
 var score
 var mob
+var waitTime
 
 
 func _ready():
+	waitTime = 1
 	randomize()
 	$MobTimer.start()
 	$MobTimer.connect("timeout", self, "on_Mob_TimeOut")
-	pass
+
 
 func on_Mob_TimeOut():
-# choose a random location on Path2D
-#$MobPath/MobSpawnLocation.set_offset(randi())
-# create a Mob instance and add it to the scene
-	while(true):
+		waitTime+=1
+		$MobTimer.set_wait_time(waitTime)
 		mob = Mob.instance()
 		add_child(mob)
-		$MobTimer.start()
-		#yield(get_tree().create_timer(5.0), "timeout")
-    # set the mob's position to a random location
-    #mob.position = $MobPath/MobSpawnLocation.position
+		#yield($MobTimer.start(5.0), "timeout")
