fb2e09463c3b6334731f4604478fbe0312bfad91
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2019-01-24 20:21:54 -0300

    coloquei o mapa certo

--- UltimateFruitDefense/scenes/mainScenes/gameScreen/Mob.gd
+++ UltimateFruitDefense/scenes/mainScenes/gameScreen/Mob.gd
@@ -5,7 +5,7 @@ var velocity = Vector2(-50, 0)
 var collision_info
 
 func _ready():
-	get_node("AnimationPlayer").play("walk")
+	$AnimationPlayer.play("walk")
 	self.set_physics_process(true)
 	select = randi()%5+1
 	#print(selec)
@@ -31,7 +31,7 @@ func _physics_process(delta):
 	collision_info = move_and_collide(velocity*delta)
 	if collision_info:
 		velocity = Vector2(0,0)
-
+		$AnimationPlayer.play("atack")
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
