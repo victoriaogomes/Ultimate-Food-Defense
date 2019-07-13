80c8983781dd7373dbfb33981b83f781dd867081
brunoclaudino <38812683+brunoclaudino@users.noreply.github.com>
2019-07-12 23:29:48 -0300

    Limitação de Movimentação
    Agora o Glóbulo está limitado a se movimentar até as bordas delimitadas

--- UltimateFruitDefense/scenes/mainScenes/gameScreen/Glob.gd
+++ UltimateFruitDefense/scenes/mainScenes/gameScreen/Glob.gd
@@ -23,10 +23,8 @@ func _ready():
 
 func on_TimeOut():
 	if(game_control.target!=null):
-		pos = get_node("/root/Node2D/Glob")
-		#pos = get_node("/root/Node2D/Glob/edge") #Dentro do Glob
-		spwan_bullet(pos)
-		
+		pos = get_node("/root/Node2D/Glob") #Pega a posição atual do Glóbulo
+		spwan_bullet(pos) # Essa posição é usada como referência para o spawn do projétil
 
 
 func _process(delta):
@@ -35,12 +33,19 @@ func _process(delta):
 	swap_animation(delta)
 
 func _physics_process(delta): #a todo frame essa função fica esperando os comandos para fazer o personagem se mecher
+	var temp = get_node("/root/Node2D/Glob")
 	if Input.is_action_pressed("ui_down"): # Se precionar seta direcionar para baixo
-		motion.y = 100 # Vetor movimento do globulo recebe 100 para ele descer no plano
+		if temp.position.y < 5.87 :
+			motion.y = 100 # Vetor movimento do globulo recebe 100 para ele descer no plano
+		else: # Caso contrário
+			motion.y = 0 # Vetor movimento é igual a zero, Glóbulo parado
 	elif Input.is_action_pressed("ui_up"): # Se precionar seta direcionar para cima
-		motion.y = -100 # Vetor movimento do glóbulo recebe -100 e sobe no plano
-	else: # Caso contrário
-		motion.y = 0 # Vetor movimento é igual a zero, Glóbulo parado
+		if temp.position.y > -24.429:
+			motion.y = -100 # Vetor movimento do glóbulo recebe -100 e sobe no plano
+		else: # Caso contrário
+			motion.y = 0 # Vetor movimento é igual a zero, Glóbulo parado
+	else:
+		motion.y = 0
 	move_and_slide(motion*(delta*40)) # Função nativa do godot para movimentar
 
 #acha o melhor inimigo e passa a instâcia dele para um variável no outro script
@@ -64,53 +69,54 @@ func set_wait_time(time):
 
 #Função Que troca a animação do Glóbulo baseada na posição do inimigo
 func swap_animation(delta):
-	if game_control.target != null: #Se houver um inimigo
-		target_position = game_control.target.position #Captura a posicação do inimigo
+	#if game_control.target != null: #Se houver um inimigo
+		#target_position = game_control.target.position #Captura a posicação do inimigo
 		#Verifica qual são as coordenadas do inimigo para decidir qual o melhor ângulo de sprite para atirar
-		if target_position.y < -111:
-			if target_position.x <-91:
-				anim = "shootUp2"
+		#if target_position.y < -111:
+			#if target_position.x <-91:
+				#anim = "shootUp2"
 				#pos = get_node("/root/Node2D/PositionUp2")
-			else:
-				anim = "shootUp1"
+			#else:
+				#anim = "shootUp1"
 				#pos = get_node("/root/Node2D/PositionUp1")
-		elif target_position.y < -48 && target_position.y > -91:
-			anim = "shootCenter"
+		#elif target_position.y < -48 && target_position.y > -91:
+			#anim = "shootCenter"
 			#pos = get_node("/root/Node2D/PositionCenter")
-		elif target_position.y < 1.8 && target_position.y > -48:
-			if target_position.x < -94:
-				anim = "shootDown1"
+		#elif target_position.y < 1.8 && target_position.y > -48:
+			#if target_position.x < -94:
+				#anim = "shootDown1"
 				#pos = get_node("/root/Node2D/PositionDown1")
-			else:
-				anim = "shootCenter"
+			#else:
+				#anim = "shootCenter"
 				#pos = get_node("/root/Node2D/PositionCenter")
-		elif target_position.y < 60 && target_position.y > 1.8:
-			if target_position.x < -170:
-				anim = "shootDown2"
+		#elif target_position.y < 60 && target_position.y > 1.8:
+			#if target_position.x < -170:
+				#anim = "shootDown2"
 				#pos = get_node("/root/Node2D/PositionDown2")
-			else:
-				anim = "shootDown1"
+			#else:
+				#anim = "shootDown1"
 				#pos = get_node("/root/Node2D/PositionDown1")
-		elif target_position.y > 60:
-			if target_position.x < -58:
-				anim = "shootDown2"
+		#elif target_position.y > 60:
+			#if target_position.x < -58:
+				#anim = "shootDown2"
 				#pos = get_node("/root/Node2D/PositionDown2")
-			else:
-				anim = "shootDown1"
+			#else:
+				#anim = "shootDown1"
 				#pos = get_node("/root/Node2D/PositionDown1")
-		$AnimatedSprite.play(anim)
-	else: #Se não houver inimigos ele mantém o sprite principal
-		$AnimatedSprite.play("idle")
+		#$AnimatedSprite.play(anim)
+	#else: #Se não houver inimigos ele mantém o sprite principal
+		#$AnimatedSprite.play("idle")
+	pass
 
 #Função que cria o objeto bullet (projétil do jogo)
 func spwan_bullet(pos):
-	var clone_bullet = bullet.instance()
-	get_parent().add_child(clone_bullet)
-	clone_bullet.add_to_group("bullet")
-	clone_bullet.position.x = pos.position.x + 4.44
-	clone_bullet.position.y = pos.position.y + 4.462
-	if(configuration.sound_effects):
-		configuration.gun_sound.play()
+	var clone_bullet = bullet.instance() # Cria um instância da scena de bullet
+	get_parent().add_child(clone_bullet) # Add o clone como nó filho
+	clone_bullet.add_to_group("bullet") # Add ao grupo bullet para o comportamento
+	clone_bullet.position.x = pos.position.x + 4.44 # Nesse ponto o projétil recebe a posição do
+	clone_bullet.position.y = pos.position.y + 4.462 # Glóbulo e ajusta para a ponta da arma
+	if(configuration.sound_effects): #Verifica se nas configurações o som está ativado
+		configuration.gun_sound.play() # Som do lançamento do projétil
 
 func _on_tempo_timeout():
 	game_control.wait_time = 2
